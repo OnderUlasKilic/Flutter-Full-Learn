a9123d431c9c0c5b8cb6ee40bed6fec6a3e2c4d7
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-02-03 02:52:37 +0300

    Advance text features  Material Design&style  Material App  Container ve SizedBox

---
+++ lib/101/text_learn_view.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class TextLearnView extends StatelessWidget {
+  TextLearnView({super.key, this.userName});
+
+  final String name = "Veli";
+  final String? userName;
+  final ProjectKeys keys = ProjectKeys();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Wellcome $name ${name.length}",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.right,
+            style: TextStyle(
+                wordSpacing: 2,
+                decoration: TextDecoration.underline,
+                fontStyle: FontStyle.italic,
+                color: Colors.lime,
+                letterSpacing: 2,
+                fontSize: 16,
+                fontWeight: FontWeight.w600),
+          ),
+          Text(
+            "Hello $name ${name.length}",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.right,
+            style: ProjectStyles.wellcomeStyle,
+          ),
+          Text(
+            "Hello $name ${name.length}",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.right,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall
+                ?.copyWith(color: ProjectColors.wellcomeColor),
+          ),
+          Text(userName ?? " "),
+          Text(keys.wellcome),
+        ],
+      )),
+    );
+  }
+}
+
+class ProjectStyles {
+  static TextStyle wellcomeStyle = TextStyle(
+      wordSpacing: 2,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.italic,
+      color: Colors.lime,
+      letterSpacing: 2,
+      fontSize: 16,
+      fontWeight: FontWeight.w600);
+}
+
+class ProjectColors {
+  static Color wellcomeColor = Colors.red;
+}
+
+class ProjectKeys {
+  final String wellcome = "Merhaba";
+}
