2c2b146c4851f7f68a3f20f10368e72a21ee16fc
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-02-09 19:59:36 +0300

    card padding

--- lib/101/padding_learn.dart
+++ lib/101/padding_learn.dart
@@ -7,20 +7,30 @@ class PaddingLearn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.white,
-            height: 100,
-            width: 200,
-          ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            width: 200,
-          ),
-        ],
+      body: Padding(
+        padding: ProjecPadding.pagePaddingVertical,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Container(color: Colors.white, height: 100),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Container(color: Colors.white, height: 100),
+            ),
+            const Padding(
+              padding: ProjecPadding.pagePAddingRightOnly,
+              child: Text("Ali"),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+class ProjecPadding {
+  static const pagePaddingVertical = EdgeInsets.symmetric(vertical: 10);
+  static const pagePAddingRightOnly = EdgeInsets.only(right: 20);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_full_learn/101/app_bar.dart';
 import 'package:flutter_full_learn/101/button_learn.dart';
+import 'package:flutter_full_learn/101/card_learn.dart';
 import 'package:flutter_full_learn/101/color_learn.dart';
 
 import 'package:flutter_full_learn/101/container_sized_box_learn.dart';
@@ -25,6 +26,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
+          cardTheme: CardTheme(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          ),
           colorScheme: ThemeData.dark().copyWith().colorScheme.copyWith(
                 error: ColorsItems().sulu,
               ),
@@ -36,7 +41,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.transparent,
             elevation: 0,
           )),
-      home: PaddingLearn(),
+      home: const CardLearn(),
     );
   }
 }
