2e48329946fe2870ad4de3622427b8d93606086a
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-02-11 00:25:57 +0300

    Applied review changes

--- lib/demos/note_demos_view.dart
+++ lib/demos/note_demos_view.dart
@@ -1,38 +1,37 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_full_learn/101/image_learn.dart';
 
 class NoteDemos extends StatelessWidget {
   const NoteDemos({super.key});
-  final _title = "Create your first note";
-  final _description = "Add a note";
-  final _createNote = "Create a Note";
-  final _importNotes = "İmport Notes";
+  static const _title = "Create your first note";
+  static const _description = "Add a note";
+  static const _createNote = "Create a Note";
+  static const _importNotes = "İmport Notes";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 201, 237, 255),
-      appBar: AppBar(
-          // systemOverlayStyle: SystemUiOverlayStyle.dark,
-          ),
+      appBar: AppBar(),
       body: Padding(
         padding: PaddingItems.horizantalPadding,
         child: Column(
           children: [
             PngImage(name: ImageItems().appleBookWithOutPath),
-            _TitleWidget(title: _title),
+            const _TitleWidget(title: _title),
             Padding(
               padding: PaddingItems.verticalPadding,
-              child: _SubtitleWidget(title: _description * 10),
+              child: _SubtitleWidget(
+                title: _description * 10,
+                textAlign: TextAlign.center,
+              ),
             ),
             const Spacer(),
-            _createButton(createNote: _createNote),
+            const _CreateButton(createNote: _createNote),
             TextButton(
               onPressed: () {},
-              child: Text(_importNotes,
-                  style: const TextStyle(color: Colors.black)),
+              child: const Text(_importNotes,
+                  style: TextStyle(color: Colors.black)),
             ),
             const SizedBox(
               height: ButtonHeights.buttonNormalHeight,
@@ -44,9 +43,8 @@ class NoteDemos extends StatelessWidget {
   }
 }
 
-class _createButton extends StatelessWidget {
-  const _createButton({
-    super.key,
+class _CreateButton extends StatelessWidget {
+  const _CreateButton({
     required String createNote,
   }) : _createNote = createNote;
 
@@ -72,9 +70,8 @@ class _createButton extends StatelessWidget {
 // Center text widget
 class _SubtitleWidget extends StatelessWidget {
   const _SubtitleWidget({
-    super.key,
-    this.textAlign = TextAlign.center,
     required this.title,
+    required this.textAlign,
   });
   final String title;
   final TextAlign textAlign;
@@ -92,7 +89,6 @@ class _SubtitleWidget extends StatelessWidget {
 
 class _TitleWidget extends StatelessWidget {
   const _TitleWidget({
-    super.key,
     required this.title,
   });
 
