a6e071deedcb3def33ec8da0ba996b96a0b20aa5
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-02-12 21:59:20 +0300

    Add list tile

---
+++ lib/101/indicator_learn.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+
+class IndicatorLearn extends StatelessWidget {
+  const IndicatorLearn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [CenterCircularRedProgress()],
+      ),
+      body: const LinearProgressIndicator(),
+    );
+  }
+}
+
+class CenterCircularRedProgress extends StatelessWidget {
+  const CenterCircularRedProgress({
+    super.key,
+  });
+  final redColor = Colors.red;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(
+        color: redColor,
+        strokeWidth: 10,
+        value: 0.9,
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
