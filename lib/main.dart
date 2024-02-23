2440783b2cf7a0ae867e1a298b91a1c635e32e1b
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-02-23 22:12:16 +0300

    Add lifecycle initstate

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ import 'package:flutter_full_learn/101/padding_learn.dart';
 import 'package:flutter_full_learn/101/scaffold_learn.dart';
 import 'package:flutter_full_learn/101/stack_learn.dart';
 import 'package:flutter_full_learn/101/statefull_learn.dart';
+import 'package:flutter_full_learn/101/statefull_life_cycle.dart';
 import 'package:flutter_full_learn/101/stateless_learn.dart';
 import 'package:flutter_full_learn/101/text_learn_view.dart';
 import 'package:flutter_full_learn/demos/note_demos_view.dart';
@@ -54,7 +55,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.transparent,
             elevation: 0,
           )),
-      home: const StatefullLearn(),
+      home: const StatefullLifeCycleLearn(message: "Velibb"),
     );
   }
 }
